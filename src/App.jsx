c96e676fe18b72bf17cbf573c25f72f5341e4cee
itablack <itamercy421@gmail.com>
2024-03-30 04:32:41 -0700

    commot

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-font">Emoji Search Appliation</h1>
+      <form>
+        <textarea placeholder="Type-------" roll="20"  ></textarea>
+      </form>
+    </div>
+  );
+}
+
+export default App;
